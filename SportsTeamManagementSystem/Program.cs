64093eed0785fb7db15858ae512e8ca9fd390e96
Turas <tomasz.czaplinski@cyfroweszkoly.pl>
2024-12-22 22:56:10 +0100

    Dodanie klasy Player implementującej interfejs IPlayer Utworzono klasę Player z konstruktorami i implementacją metody UpdateScore Klasa reprezentuje pojedynczego zawodnika

--- SportsTeamManagementSystem/Program.cs
+++ SportsTeamManagementSystem/Program.cs
@@ -11,6 +11,25 @@ public interface IPlayer
     void UpdateScore(int newScore);
 }
 
+public class Player : IPlayer
+{
+    public string Name { get; set; }
+    public string Position { get; set; }
+    public int Score { get; set; }
+
+    public Player(string name, string position, int score)
+    {
+        Name = name;
+        Position = position;
+        Score = score;
+    }
+
+    public void UpdateScore(int newScore)
+    {
+        Score = newScore;
+    }
+}
+
 internal class Program
 {
     public static void Main(string[] args)
